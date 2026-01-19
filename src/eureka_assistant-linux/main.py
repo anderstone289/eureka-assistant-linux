8a5e2098d6e1309f7a54aaa296d1f8fb16644b3e
andrey <chickensuper48@gmail.com>
2026-01-19 20:02:23 +0700

    Добавил несколько новых команд и пофиксил несколько возникших при разработке багов.

--- src/eureka_assistant-linux/main.py
+++ src/eureka_assistant-linux/main.py
@@ -7,6 +7,7 @@ import aiohttp
 import json
 from openai import OpenAI
 import time
+from datetime import datetime
 import webbrowser
 from importlib import resources
 from dotenv import load_dotenv, find_dotenv, set_key
@@ -20,6 +21,8 @@ dotenv_path = find_dotenv()
 if os.getenv("HELP_CALL_COUNT") is None:
     set_key(dotenv_path, "HELP_CALL_COUNT", "0")
 
+#Время сейчас
+now = datetime.now()
 
 #Инициилизация API ключа.
 API_KEY = os.getenv("API_KEY")
@@ -27,35 +30,45 @@ API_KEY = os.getenv("API_KEY")
 mode = input("Введите режим Эврики: 'AImode/StandartMode': ")
 
 #Класс для создания объекта Стандартного мода
-class StandardMode:
+class StandartMode:
     def __init__(self):
         self.comand = input("Введите команду или 'помощь' для вывода доступных команд:")
 
     @staticmethod
     #Функция для выведения доступных команд в терминал.
-    def help():
+    def comands():
         with resources.open_text("eureka_assistant-linux", "comands.txt") as file:
             content = file.read()
             print(content)
     def hello(self):
         print("Здравствуйте сэр, я эврика ваш виртуальный ассистент.")
-        time.sleep(0.5)
-        print("Введите ваше имя:"
-              "Если вы пропутстите этот пункт и нажмете 'enter' я буду называть вас 'сэр':")
-
-    """Реализовать потом когда добавлю ИИ режим"""
-    # def openfile(self):
-    #     f = open(f'{self.comand}', 'r')
-    #     return f
+        name = input("Введите ваше имя Подсказка:если вы нажмете 'enter' и ввод будет пустым я буду называть вас 'Cэр':")
     """Основной цикл"""
     def main(self):
         comand = self.comand.split()
         while True:
-            if comand[0] == "открой":
+            comand = input("Введите команду или 'помощь' для вывода доступных команд:").split()
+            if not comand:
+                continue
+            elif comand[0] == "открой":
                 webbrowser.open(self.comand[1])
                 continue
             elif comand[0] == "помощь":
-                help()
+                StandartMode.comands()
+                continue
+            elif comand[0] == "время":
+                local_time = time.localtime()
+                print(time.strftime("%H:%M:%S", local_time))
+                continue
+            elif comand[0] == "дата":
+                print(now.strftime("%d.%m.%Y %H:%M"))
+                continue
+                """Реализовать потом."""
+            # elif comand[0] == "режим":
+            #     mode = input("Введите режим Эврики: 'AImode/StandartMode': ")
+            #     continue
+            else:
+                print("Команда не распознана, сэр.")
 
 #Класс для создания объекта AI мода
 class AImode:
@@ -72,7 +85,7 @@ if __name__ == '__main__':
         ai_mode = AImode()
         ai_mode.DeepseekModel()
     elif mode == "StandartMode":
-        standart_mode = StandardMode()
+        standart_mode = StandartMode()
         help_call_count = int(os.environ.get("HELP_CALL_COUNT", 0))
         if help_call_count == 0:
             standart_mode.hello()
